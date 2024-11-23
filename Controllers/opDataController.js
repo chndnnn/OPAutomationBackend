e19bfc444936155f4e4d933bc0e51200a825a907
chndnnn <chndn136@gmail.com>
2024-11-23 14:36:46 +0530

    New API additions

--- Controllers/opDataController.js
+++ Controllers/opDataController.js
@@ -9,3 +9,52 @@ export const getAllData = async(req,res)=>{
         res.status(500).send('Server Error');
       }
 }
+
+export const insertDatatoDb = async(req,res)=>{
+  const data = req.body;
+  const client = await pool.connect();
+  let count = 0
+  try {
+    await client.query("BEGIN")
+    for (const row of data) {
+      // Assuming your table schema matches the Excel headers
+      const {
+        invoice_number,
+        batch_id,
+        invoice_received_date,
+        invoice_status,
+        type,
+        invoice_posted_date,
+        avg_days,
+      } = row;
+
+      await client.query(
+        `INSERT INTO invoice_details (invoice_number, batch_id, invoice_received_date, invoice_status, type, invoice_posted_date, avg_days) 
+        VALUES ($1, $2, $3, $4, $5, $6, $7)`,
+        [
+          invoice_number,
+          batch_id,
+          invoice_received_date,
+          invoice_status,
+          type,
+          invoice_posted_date,
+          avg_days,
+        ]
+      );
+      count++
+    }
+    await client.query("COMMIT");
+    res.status(200).send("Data inserted successfully!");
+  } catch (error) {
+    await client.query("ROLLBACK");
+    console.error("Error inserting data:", error);
+    console.log(count)
+    res.status(500).json({
+      status:"failed",
+      lineno:count,
+      message: error
+    })
+  }finally {
+    client.release(); // Release the client back to the pool
+  }
+}
--- Routers/opDataRouter.js
+++ Routers/opDataRouter.js
@@ -1,8 +1,9 @@
 import express from "express"
-import { getAllData } from "../Controllers/opDataController.js"
+import { getAllData, insertDatatoDb } from "../Controllers/opDataController.js"
 
 const router = express.Router()
 
 router.route('/opData').get(getAllData)
+router.route('/insertInvoiceDetails').post(insertDatatoDb)
 
 export default router
